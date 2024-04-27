0900d6e58565ab543058f30ae032ab1ede10a297
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2024-04-27 13:32:24 +0300

    initial commit.Create main files

---
+++ src/main/java/com/example/clearsolutiontesttask/ClearSolutionTestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.clearsolutiontesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSolutionTestTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/clearsolutiontesttask/ClearSolutionTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clearsolutiontesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
